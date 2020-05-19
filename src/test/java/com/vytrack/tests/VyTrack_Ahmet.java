a071fc42057547d6c1c00f85c514c9c03a0c138d
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-19 14:34:21 +0100

    update VyTrack_Ahmet

--- src/test/java/com/vytrack/tests/VyTrack_Ahmet.java
+++ src/test/java/com/vytrack/tests/VyTrack_Ahmet.java
@@ -10,5 +10,13 @@ public class VyTrack_Ahmet {
  2.INTELLIJ | git branch
  3.INTELLIJ | git checkout -b VyTrack_Ahmet (creates a branch as local repo)
  4 INTELLIJ | git push --set-upstream origin VyTrack_Ahmet (pushes whatever we have to the remote branch)
- 5.GITHUB | click "compare and pull request" then click "pull request" then click "
+ 5.GITHUB | click "new pull request" then compare changes between master and VyTrack_Ahmet then click
+ "Create pull request"
+ 6.INTELLIJ | git checkout master
+ 7.INTELLIJ | git pull
+
+Note: When we are in local master branch it's possible to push the changes in the local branch
+to the remote local branch ----> git push -u origin VyTrack_Ahmet
+
+ "compare and pull request" then click "pull request" then click"
  */
